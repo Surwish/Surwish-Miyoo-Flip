75e4469a41559a02c0ef88ff3108bba125ad42f3
SurwishOS <SurwishOS@gmail.com>
2025-05-16 14:50:05 +0200

    修复 GMU 退出键 - Fix GMU exit key

--- App/MusicPlayer/gmu_launcher.sh
+++ App/MusicPlayer/gmu_launcher.sh
@@ -6,11 +6,10 @@ first_run_file="/mnt/SDCARD/App/MusicPlayer/FirstRunDone"
 
 cd /mnt/SDCARD/App/MusicPlayer
 
-/mnt/SDCARD/App/PortMaster/PortMaster/gptokeyb2 "gmu.bin" -c "/mnt/SDCARD/App/MusicPlayer/gptokeyb.gptk" &
-sleep 0.4
+/mnt/SDCARD/App/PortMaster/PortMaster/gptokeyb2 "gmu.bin" -c "/mnt/SDCARD/App/MusicPlayer/trimui_smart_pro.gptk" &
 
 # To support LCD switch-Off key combination
-/mnt/SDCARD/System/bin/thd --triggers /mnt/SDCARD/App/MusicPlayer/thd.conf /dev/input/event3 &
+# /mnt/SDCARD/System/bin/thd --triggers /mnt/SDCARD/App/MusicPlayer/thd.conf /dev/input/event3 &
 
 echo 1 >/tmp/stay_awake
 HOME=/mnt/SDCARD/App/MusicPlayer /mnt/SDCARD/App/MusicPlayer/gmu.bin -c gmu.settings.conf
